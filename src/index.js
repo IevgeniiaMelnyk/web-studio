2e4e9268e09c97638caa1a622b3a28343e99f8e0
Ievgeniia Melnyk <agnikava@gmail.com>
2022-12-22 20:57:09 +0200

    add hero

---
+++ src/index.js
@@ -0,0 +1,1 @@
+import './js/modale-menu';
---
+++ src/js/modale-menu.js
@@ -0,0 +1,12 @@
+const refs = {
+    openMenuBtn: document.querySelector('.mobile-menu__open-btn'),
+    closeMenuBtn: document.querySelector('.mobile-menu__close-btn'),
+    menu: document.querySelector('.mobile-menu'),
+};
+
+refs.openMenuBtn.addEventListener('click', toggleMenu);
+refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+ function toggleMenu() {
+    refs.menu.classList.toggle('is-open');
+  }
